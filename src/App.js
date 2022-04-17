07600661ab79a3abf2f2fcc918e5e41bb0c1d893
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-04-17 17:50:03 +0530

    feat : single video page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { VideoListing } from "./Pages/Video-Listing/VideoListing";
+import { VideoPlayer } from "./Pages/Video-player/videoPlayer";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<VideoListing />} />
+        <Route path="/video/:videoId" element={<VideoPlayer />} />
       </Routes>
     </div>
   );
