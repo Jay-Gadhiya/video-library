a1e2f79a6c9e5da29de7e200827a408cec6f6c97
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-04-20 18:38:40 +0530

    style : responsive

--- src/Components/horizontalCard/horizontalCard.jsx
+++ src/Components/horizontalCard/horizontalCard.jsx
@@ -3,6 +3,7 @@ import { FaTrashAlt } from 'react-icons/fa';
 import { getUrl } from "../../Utility-functions/getUrl";
 import { useAuth } from "../../context/authentication-context";
 import { useData } from "../../context/dataStore";
+import { Link } from "react-router-dom";
 
 export const HorizontalCard = ({ videos, deleteHandler }) => {
 
@@ -12,14 +13,17 @@ export const HorizontalCard = ({ videos, deleteHandler }) => {
 
     return (
         <div className="horizontal-card-container">
-            <div className="horizontal-img-wrapper">
-                <img className="horizontal-card-img" src={likedImg} alt="img" />
-                <span className="time-stamp">{ videos.duration }</span>
-            </div>
-            <div className="horizontal-card-body">
-                <p className="horizontal-card-title">{ videos.title }</p>
-                <p className="horizontal-card-channel">{ videos.channel }</p>
-            </div>
+            <Link to={`/video/${videos._id}`} >
+                <div className="horizontal-img-wrapper">
+                    <img className="horizontal-card-img" src={likedImg} alt="img" />
+                    <span className="time-stamp">{ videos.duration }</span>
+                </div>
+            </Link>
+                <div className="horizontal-card-body">
+                    <p className="horizontal-card-title">{ videos.title }</p>
+                    <p className="horizontal-card-channel">{ videos.channel }</p>
+                </div>
+
             <FaTrashAlt onClick={() => deleteHandler(videos, authState, dataStoreDispatch)} className="trash-icon" />
         </div>
     )
--- src/context/authentication-context.jsx
+++ src/context/authentication-context.jsx
@@ -5,11 +5,9 @@ const AuthContext = createContext();
 const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({children}) => {
-
-    const [authState, authDispatch] = useReducer(authReducer, { token : null });
-    const [user, setUser] = useState("");
-
+    
     const getToken = localStorage.getItem("token");
+    const [authState, authDispatch] = useReducer(authReducer, { token : getToken });
     const getUser = JSON.parse(localStorage.getItem("user"));
 
     useEffect(() => {
